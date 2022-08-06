1ed8a84cceeb4969ce4f7e7e52209e23f2db94e8
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-08-06 15:21:35 -0400

    Fix mixins.

--- src/main/java/net/enderturret/patchedmod/util/MixinCallbacks.java
+++ src/main/java/net/enderturret/patchedmod/util/MixinCallbacks.java
@@ -20,7 +20,8 @@ import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.ResourcePackFileNotFoundException;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
-import net.minecraft.server.packs.resources.SimpleResource;
+import net.minecraft.server.packs.resources.FallbackResourceManager.PackEntry;
+import net.minecraft.server.packs.resources.Resource;
 
 import net.enderturret.patched.Patches;
 import net.enderturret.patched.exception.PatchingException;
@@ -30,32 +31,16 @@ import net.enderturret.patchedmod.Patched;
 /**
  * <p>Handles callbacks from mixins -- what you would expect given the name.</p>
  * <p>Specifically, this handles actually patching things.</p>
- * @see #loadResource(FallbackResourceManager, String, ResourceLocation, InputStream, InputStream)
  * @author EnderTurret
  */
 public class MixinCallbacks {
 
-	/**
-	 * Handles patching a resource, if possible.
-	 * @param manager The resource manager that this resource is from.
-	 * @param sourceName The resource/data pack that the resource is from.
-	 * @param name The location of the resource.
-	 * @param resource The contents of the resource itself.
-	 * @param metadata The contents of the resource's .mcmeta file, if present. {@code null} means that no such .mcmeta file exists.
-	 * @return The patched resource.
-	 */
-	@SuppressWarnings("resource")
-	public static SimpleResource loadResource(FallbackResourceManager manager, String sourceName, ResourceLocation name, InputStream resource, @Nullable InputStream metadata) {
-		if (Patched.canBePatched(name)) {
-			final PackType type = manager.type;
-
-			resource = patch(manager, sourceName, type, name, resource);
-
-			if (metadata != null)
-				metadata = patch(manager, sourceName, type, new ResourceLocation(name.getNamespace(), name.getPath() + ".mcmeta"), metadata);
-		}
-
-		return new SimpleResource(sourceName, name, resource, metadata);
+	public static Resource.IoSupplier<InputStream> chain(Resource.IoSupplier<InputStream> delegate, FallbackResourceManager manager, ResourceLocation name, PackResources origin) {
+		return () -> {
+			try (InputStream ret = delegate.get()) {
+				return patch(manager, origin, manager.type, name, ret);
+			}
+		};
 	}
 
 	/**
@@ -66,7 +51,7 @@ public class MixinCallbacks {
 	 * @param stream The data stream.
 	 * @return A new stream containing the patched data.
 	 */
-	private static InputStream patch(FallbackResourceManager manager, String sourceName, PackType type, ResourceLocation name, InputStream stream) {
+	private static InputStream patch(FallbackResourceManager manager, PackResources from, PackType type, ResourceLocation name, InputStream stream) {
 		if (stream == null) return stream;
 
 		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -85,7 +70,7 @@ public class MixinCallbacks {
 		try {
 			final JsonElement elem = JsonParser.parseString(json);
 
-			patch(manager, sourceName, type, name, elem);
+			patch(manager, from, type, name, elem);
 
 			json = PatchUtil.GSON.toJson(elem);
 			bytes = json.getBytes(StandardCharsets.UTF_8);
@@ -105,19 +90,19 @@ public class MixinCallbacks {
 	 * @param elem The Json data to patch.
 	 */
 	@SuppressWarnings("resource")
-	private static void patch(FallbackResourceManager manager, String sourceName, PackType type, ResourceLocation name, JsonElement elem) {
+	private static void patch(FallbackResourceManager manager, PackResources from, PackType type, ResourceLocation name, JsonElement elem) {
 		final ResourceLocation patchName = new ResourceLocation(name.getNamespace(), name.getPath() + ".patch");
 
 		for (int i = manager.fallbacks.size() - 1; i >= 0; i--) {
-			final PackResources pack = manager.fallbacks.get(i);
+			final PackEntry pack = manager.fallbacks.get(i);
 
-			if (hasPatches(pack) && pack.hasResource(type, patchName)) {
+			if (hasPatches(pack) && pack.resources().hasResource(type, patchName)) {
 				final String patchJson;
 
-				try (InputStream patchStream = pack.getResource(type, patchName)) {
+				try (InputStream patchStream = pack.resources().getResource(type, patchName)) {
 					patchJson = PatchUtil.readString(patchStream);
 				} catch (IOException e) {
-					Patched.LOGGER.warn("Failed to read patch {} from {}:", patchName, pack.getName(), e);
+					Patched.LOGGER.warn("Failed to read patch {} from {}:", patchName, pack.name(), e);
 					continue;
 				}
 
@@ -126,21 +111,21 @@ public class MixinCallbacks {
 				try {
 					patch = Patches.readPatch(PatchUtil.GSON, patchJson);
 				} catch (JsonParseException e) {
-					Patched.LOGGER.warn("Failed to parse patch {} from {}:", patchName, pack.getName(), e);
+					Patched.LOGGER.warn("Failed to parse patch {} from {}:", patchName, pack.name(), e);
 					continue;
 				}
 
 				try {
-					Patched.LOGGER.info("Applying patch {} from {}.", patchName, pack.getName());
+					Patched.LOGGER.info("Applying patch {} from {}.", patchName, pack.name());
 					patch.patch(elem, PatchUtil.CONTEXT);
 				} catch (PatchingException e) {
-					Patched.LOGGER.warn("Failed to apply patch {} from {}:\n{}", patchName, pack.getName(), e.toString());
+					Patched.LOGGER.warn("Failed to apply patch {} from {}:\n{}", patchName, pack.name(), e.toString());
 				} catch (Exception e) {
-					Patched.LOGGER.warn("Failed to apply patch {} from {}:", patchName, pack.getName(), e);
+					Patched.LOGGER.warn("Failed to apply patch {} from {}:", patchName, pack.name(), e);
 				}
 			}
 
-			if (pack.getName().equals(sourceName))
+			if (pack.resources() == from)
 				break;
 		}
 	}
@@ -148,23 +133,24 @@ public class MixinCallbacks {
 	/**
 	 * Determines whether the given pack has patches.
 	 * If necessary, the pack may be {@linkplain IPatchingPackResources#initialized() initialized}.
-	 * @param resources
+	 * @param pack
 	 * @return
 	 */
-	private static boolean hasPatches(PackResources resources) {
-		if (!(resources instanceof IPatchingPackResources patching))
+	@SuppressWarnings("resource")
+	private static boolean hasPatches(PackEntry pack) {
+		if (!(pack.resources() instanceof IPatchingPackResources patching))
 			return false;
 
 		if (!patching.initialized())
 			synchronized (patching) {
 				if (!patching.initialized()) {
-					try (InputStream is = resources.getRootResource("pack.mcmeta")) {
+					try (InputStream is = pack.resources().getRootResource("pack.mcmeta")) {
 						final String json = PatchUtil.readString(is);
 						final JsonElement elem = JsonParser.parseString(json);
 
 						if (elem instanceof JsonObject o
-								&& o.get("pack") instanceof JsonObject pack
-								&& pack.get("patched:has_patches") instanceof JsonPrimitive prim
+								&& o.get("pack") instanceof JsonObject packObj
+								&& packObj.get("patched:has_patches") instanceof JsonPrimitive prim
 								&& prim.isBoolean())
 							patching.setHasPatches(prim.getAsBoolean());
 
@@ -172,12 +158,12 @@ public class MixinCallbacks {
 					} catch (ResourcePackFileNotFoundException e) {
 						patching.setHasPatches(false);
 					} catch (Exception e) {
-						Patched.LOGGER.error("Failed to read pack.mcmeta in {}:", resources.getName(), e);
+						Patched.LOGGER.error("Failed to read pack.mcmeta in {}:", pack.name(), e);
 						patching.setHasPatches(false);
 					}
 
 					if (patching.hasPatches())
-						Patched.LOGGER.info("Enabled patching for {} ({}).", resources.getName(), resources);
+						Patched.LOGGER.info("Enabled patching for {} ({}).", pack.name(), pack.resources());
 				}
 			}
 
