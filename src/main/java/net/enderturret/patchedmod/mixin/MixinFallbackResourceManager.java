692782952aaa7815f70425546a8507df108f10d6
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-08-06 13:39:14 -0400

    Theoretically fix the mixins.

--- src/main/java/net/enderturret/patchedmod/mixin/MixinFallbackResourceManager.java
+++ src/main/java/net/enderturret/patchedmod/mixin/MixinFallbackResourceManager.java
@@ -5,25 +5,29 @@ import java.io.InputStream;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
-import net.minecraft.server.packs.resources.SimpleResource;
+import net.minecraft.server.packs.resources.Resource;
 
 import net.enderturret.patchedmod.util.MixinCallbacks;
 
 /**
  * <p>This mixin implements the functionality for actually patching resources.</p>
  * <p>This is done by redirecting the {@code new SimpleResource(...)} invocations to
- * {@link MixinCallbacks#loadResource(FallbackResourceManager, String, ResourceLocation, InputStream, InputStream)}.</p>
+ * {@link MixinCallbacks#chain(Resource.IoSupplier, FallbackResourceManager, ResourceLocation, PackResources)}.</p>
  * @author EnderTurret
  */
 @Mixin(FallbackResourceManager.class)
 public abstract class MixinFallbackResourceManager {
 
-	@Redirect(at = @At(value = "NEW", target = "net/minecraft/server/packs/resources/SimpleResource"), method = { "getResource", "getResources" })
-	private SimpleResource replaceResource(String sourceName, ResourceLocation location, InputStream resources, @Nullable InputStream metadata) {
-		return MixinCallbacks.loadResource((FallbackResourceManager) (Object) this, sourceName, location, resources, metadata);
+	@Inject(at = @At(value = "RETURN"), method = "createResourceGetter")
+	private void replaceResource(ResourceLocation location, PackResources pack, CallbackInfoReturnable<Resource.IoSupplier<InputStream>> cir) {
+		final var sup = cir.getReturnValue();
+		cir.setReturnValue(MixinCallbacks.chain(sup, (FallbackResourceManager) (Object) this, location, pack));
 	}
 }
