cae55dedd48c8c19907676ec166bab08e73ef232
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2022-08-08 16:55:20 -0400

    Add support for the new custom test conditions.

--- src/main/java/net/enderturret/patchedmod/Patched.java
+++ src/main/java/net/enderturret/patchedmod/Patched.java
@@ -3,15 +3,19 @@ package net.enderturret.patchedmod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.JsonElement;
+
 import net.minecraft.resources.ResourceLocation;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.IExtensionPoint.DisplayTest;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkConstants;
 
+import net.enderturret.patched.exception.PatchingException;
 import net.enderturret.patchedmod.command.PatchedCommand;
 import net.enderturret.patchedmod.util.MixinCallbacks;
 import net.enderturret.patchedmod.util.PatchUtil;
@@ -31,10 +35,19 @@ public class Patched {
 	public Patched() {
 		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (version, network) -> true));
 		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
+
+		PatchedTestConditions.registerSimple(new ResourceLocation(MOD_ID, "mod_loaded"), value -> ModList.get().isLoaded(assertIsString("mod_loaded", value)));
 	}
 
 	private void registerCommands(RegisterCommandsEvent e) {
 		e.getDispatcher().register(PatchedCommand.create(false, src -> src.getServer().getResourceManager()));
+	}
+
+	private static String assertIsString(String id, JsonElement value) {
+		if (!value.isJsonPrimitive() || !value.getAsJsonPrimitive().isString())
+			throw new PatchingException(id + ": value must be a string");
+
+		return value.getAsString();
 	}
 
 	/**
