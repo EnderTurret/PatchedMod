b63dc2359b7c059e1eece719dbb78abcac150a04
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2023-06-09 22:49:05 -0400

    Similar PatchUtil changes as on the Fabric branch.

--- src/main/java/net/enderturret/patchedmod/util/PatchUtil.java
+++ src/main/java/net/enderturret/patchedmod/util/PatchUtil.java
@@ -55,12 +55,20 @@ public final class PatchUtil {
 		// We have to use a dot for the VanillaPackResources because otherwise LinkFileSystem's path handling gets a little concerned.
 		try {
 			final boolean vanilla = pack instanceof VanillaPackResources;
-			pack.listResources(type, vanilla ? "." : "", namespace, (loc, io) -> {
-				if (filter.test(loc))
+			final String fakeNamespace = vanilla ? "." : "";
+			final String fakePath = namespace;
+
+			pack.listResources(type, fakeNamespace, fakePath, (loc, io) -> {
+				if (filter.test(loc)) {
+					final ResourceLocation renamed;
+
 					// We do have to fix the resource location though.
 					// :minecraft/something → minecraft:something
 					// :../minecraft/something → minecraft:something
-					ret.add(new ResourceLocation(namespace, loc.getPath().substring((vanilla ? "../".length() : 0) + namespace.length() + 1)));
+					renamed = new ResourceLocation(namespace, loc.getPath().substring((vanilla ? "../".length() : 0) + namespace.length() + 1));
+
+					ret.add(renamed);
+				}
 			});
 		} catch (Exception e) {
 			Patched.arch().logger().error("Exception listing resources:", e);
