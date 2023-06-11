a393895f36b7b01656b1877999889fb510b3f2e6
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2023-06-10 20:03:32 -0400

    Setup multi-platform workspace.

---
+++ fabric/src/main/java/net/enderturret/patchedmod/fabric/FabricArchitecture.java
@@ -0,0 +1,86 @@
+package net.enderturret.patchedmod.fabric;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.fabric.api.resource.ModResourcePack;
+import net.fabricmc.fabric.impl.resource.loader.ModNioResourcePack;
+import net.fabricmc.loader.api.FabricLoader;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.PackType;
+
+import net.enderturret.patchedmod.mixin.fabric.GroupResourcePackAccess;
+import net.enderturret.patchedmod.util.env.IArchitecture;
+
+final class FabricArchitecture implements IArchitecture {
+
+	private final Logger logger = LoggerFactory.getLogger("Patched");
+
+	@Override
+	public Logger logger() {
+		return logger;
+	}
+
+	@Override
+	public boolean isPhysicalClient() {
+		return PatchedFabric.physicalClient;
+	}
+
+	@Override
+	public boolean isModLoaded(String modId) {
+		return FabricLoader.getInstance().isModLoaded(modId);
+	}
+
+	@Override
+	public PackOutput getPackOutput(DataGenerator generator) {
+		return generator.vanillaPackOutput;
+	}
+
+	@Override
+	public String getName(PackResources pack) {
+		return pack instanceof ModResourcePack mod ? "mod/" + mod.getFabricModMetadata().getName() : pack.packId();
+	}
+
+	@Override
+	public boolean isGroup(PackResources pack) {
+		return pack instanceof GroupResourcePackAccess;
+	}
+
+	@Override
+	public Collection<PackResources> getChildren(PackResources pack) {
+		return pack instanceof GroupResourcePackAccess grpa ? transform(grpa.getPacks()) : List.of();
+	}
+
+	@Override
+	public Collection<PackResources> getFilteredChildren(PackResources pack, PackType type, ResourceLocation file) {
+		return pack instanceof GroupResourcePackAccess grpa ? transform(grpa.getNamespacedPacks().getOrDefault(file.getNamespace(), List.of())) : List.of();
+	}
+
+	private static Collection<PackResources> transform(List<ModResourcePack> list) {
+		return list.stream().map(mrp -> (PackResources) mrp).toList();
+	}
+
+	@Override
+	public boolean needsSwapNamespaceAndPath(PackResources pack) {
+		// Fabric implementations surprisingly throw no errors, unlike Minecraft.
+		return !isGroup(pack) && !(pack instanceof ModNioResourcePack);
+	}
+
+	@Override
+	public Function<ResourceLocation, ResourceLocation> getRenamer(PackResources pack, String namespace) {
+		// GroupResourcePack and ModNioResourcePack
+		if (!needsSwapNamespaceAndPath(pack)) return rl -> rl;
+		// PathPackResources:      :minecraft/something → minecraft:something
+		// FilePackResources is handled separately.
+		// VanillaPackResources:  .:minecraft/something → minecraft:something
+		return rl -> new ResourceLocation(namespace, rl.getPath().substring(namespace.length() + 1));
+	}
+}
---
+++ fabric/src/main/java/net/enderturret/patchedmod/mixin/fabric/GroupResourcePackAccess.java
@@ -0,0 +1,20 @@
+package net.enderturret.patchedmod.mixin.fabric;
+
+import java.util.List;
+import java.util.Map;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.fabricmc.fabric.api.resource.ModResourcePack;
+import net.fabricmc.fabric.impl.resource.loader.GroupResourcePack;
+
+@Mixin(GroupResourcePack.class)
+public interface GroupResourcePackAccess {
+
+	@Accessor(remap = false)
+	public List<ModResourcePack> getPacks();
+
+	@Accessor(remap = false)
+	public Map<String, List<ModResourcePack>> getNamespacedPacks();
+}
