33851821b4eb251f4d3a8e60384bb277367f57e1
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2023-06-22 16:53:40 -0400

    Copy over the IPlatform methods from 1.20.

--- fabric/src/main/java/net/enderturret/patchedmod/fabric/FabricPlatform.java
+++ fabric/src/main/java/net/enderturret/patchedmod/fabric/FabricPlatform.java
@@ -1,9 +1,17 @@
 package net.enderturret.patchedmod.fabric;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.fabric.api.resource.ModResourcePack;
 import net.fabricmc.loader.api.FabricLoader;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.PackType;
 
 import net.enderturret.patchedmod.util.env.IPlatform;
 
@@ -24,5 +32,29 @@ public final class FabricPlatform implements IPlatform {
 	@Override
 	public boolean isModLoaded(String modId) {
 		return FabricLoader.getInstance().isModLoaded(modId);
+	}
+
+	@Override
+	public String getName(PackResources pack) {
+		return pack instanceof ModResourcePack mod ? "mod/" + mod.getFabricModMetadata().getName() : pack.getName();
+	}
+
+	@Override
+	public boolean isGroup(PackResources pack) {
+		return false;//pack instanceof GroupResourcePackAccess;
+	}
+
+	@Override
+	public Collection<PackResources> getChildren(PackResources pack) {
+		return List.of();//pack instanceof GroupResourcePackAccess grpa ? transform(grpa.getPacks()) : List.of();
+	}
+
+	@Override
+	public Collection<PackResources> getFilteredChildren(PackResources pack, PackType type, ResourceLocation file) {
+		return List.of();//pack instanceof GroupResourcePackAccess grpa ? transform(grpa.getNamespacedPacks().getOrDefault(file.getNamespace(), List.of())) : List.of();
+	}
+
+	private static Collection<PackResources> transform(List<ModResourcePack> list) {
+		return list.stream().map(mrp -> (PackResources) mrp).toList();
 	}
 }
--- forge/src/main/java/net/enderturret/patchedmod/forge/ForgePlatform.java
+++ forge/src/main/java/net/enderturret/patchedmod/forge/ForgePlatform.java
@@ -1,12 +1,23 @@
 package net.enderturret.patchedmod.forge;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.PackType;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.resource.PathPackResources;
 
+import net.enderturret.patchedmod.mixin.forge.DelegatingPackResourcesAccess;
 import net.enderturret.patchedmod.util.env.IPlatform;
 
 public final class ForgePlatform implements IPlatform {
@@ -26,5 +37,34 @@ public final class ForgePlatform implements IPlatform {
 	@Override
 	public boolean isModLoaded(String modId) {
 		return ModList.get().isLoaded(modId);
+	}
+
+	@Override
+	public String getName(PackResources pack) {
+		return pack instanceof PathPackResources ppp ? "mod/" + findModNameFromModFile(pack.getName()) : pack.getName();
+	}
+
+	private static String findModNameFromModFile(String modFile) {
+		return ModList.get().getModFiles()
+				.stream()
+				.filter(mfi -> modFile.equals(mfi.getFile().getFileName()))
+				.flatMap(mfi -> mfi.getMods().stream())
+				.map(IModInfo::getDisplayName)
+				.findFirst().orElse(modFile);
+	}
+
+	@Override
+	public boolean isGroup(PackResources pack) {
+		return pack instanceof DelegatingPackResourcesAccess;
+	}
+
+	@Override
+	public Collection<PackResources> getChildren(PackResources pack) {
+		return pack instanceof DelegatingPackResourcesAccess dpra ? dpra.getDelegates() : List.of();
+	}
+
+	@Override
+	public Collection<PackResources> getFilteredChildren(PackResources pack, PackType type, ResourceLocation file) {
+		return pack instanceof DelegatingPackResourcesAccess dpra ? dpra.callGetCandidatePacks(type, file) : List.of();
 	}
 }
