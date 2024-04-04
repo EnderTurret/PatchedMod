53ad5f4b18af878e65b6d4dc6ba243f6bf9af3c0
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2024-04-04 02:36:40 -0400

    Probably fix mixins.

--- common/src/main/java/net/enderturret/patchedmod/mixin/MixinPackConfig.java
+++ common/src/main/java/net/enderturret/patchedmod/mixin/MixinPackConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.server.WorldLoader;
 import net.minecraft.server.packs.PackResources;
@@ -17,7 +18,7 @@ public abstract class MixinPackConfig {
 
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;openAllSelected()Ljava/util/List;"),
 			method = { "createResourceManager" })
-	private void patched$setupServerPatchTargetManager(List<PackResources> packsByPriority) {
+	private void patched$setupServerPatchTargetManager(List<PackResources> packsByPriority, CallbackInfoReturnable<?> cir) {
 		MixinCallbacks.setupTargetManager(PackType.SERVER_DATA, packsByPriority);
 	}
 }
--- common/src/main/java/net/enderturret/patchedmod/mixin/client/MixinMinecraft.java
+++ common/src/main/java/net/enderturret/patchedmod/mixin/client/MixinMinecraft.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.PackResources;
@@ -15,17 +16,9 @@ import net.enderturret.patchedmod.util.MixinCallbacks;
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
 
-	// These are both separate because I'm pretty sure Mixin will defenestrate me if I have a static injector on an instance target.
-
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;openAllSelected()Ljava/util/List;"),
-			method = { "reloadResourcePacks" })
-	private void patched$setupClientPatchTargetManager(List<PackResources> packsByPriority) {
-		MixinCallbacks.setupTargetManager(PackType.CLIENT_RESOURCES, packsByPriority);
-	}
-
-	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;openAllSelected()Ljava/util/List;"),
-			method = { "<init>" })
-	private static void patched$setupClientPatchTargetManagerStatic(List<PackResources> packsByPriority) {
+			method = { "<init>", "reloadResourcePacks" })
+	private void patched$setupClientPatchTargetManager(List<PackResources> packsByPriority, CallbackInfo ci) {
 		MixinCallbacks.setupTargetManager(PackType.CLIENT_RESOURCES, packsByPriority);
 	}
 }
