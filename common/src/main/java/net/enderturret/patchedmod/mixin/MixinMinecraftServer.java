51b2463bb1f7f0a34670d8109ef049ca57023a0d
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2024-12-23 17:54:37 -0500

    Fix `MinecraftServer` mixin not applying.

--- common/src/main/java/net/enderturret/patchedmod/mixin/MixinMinecraftServer.java
+++ common/src/main/java/net/enderturret/patchedmod/mixin/MixinMinecraftServer.java
@@ -1,10 +1,15 @@
 package net.enderturret.patchedmod.mixin;
 
+import java.util.List;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.MultiPackResourceManager;
 
@@ -18,15 +23,16 @@ import net.enderturret.patchedmod.internal.flow.DynamicPatches;
 @Mixin(MinecraftServer.class)
 public abstract class MixinMinecraftServer {
 
-	@ModifyVariable(
+	@WrapOperation(
 			at = @At(
 					value = "NEW",
 					target = "Lnet/minecraft/server/packs/resources/MultiPackResourceManager;"
 					),
 			method = "*"
 	)
-	private MultiPackResourceManager patched$setupServerPatchTargetManager(MultiPackResourceManager manager) {
-		DynamicPatches.setupTargetManager(PackType.SERVER_DATA, manager.listPacks().toList());
-		return manager;
+	private MultiPackResourceManager patched$setupServerPatchTargetManager(
+			PackType type, List<PackResources> packs, Operation<MultiPackResourceManager> original) {
+		DynamicPatches.setupTargetManager(type, packs);
+		return original.call(type, packs);
 	}
 }
