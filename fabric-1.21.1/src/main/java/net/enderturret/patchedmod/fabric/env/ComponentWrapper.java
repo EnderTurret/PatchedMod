b97e5606a7e1e72b2d148efc2dc76c965a795f81
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2026-02-20 18:12:11 -0500

    Set up NeoForge and Fabric 1.21.1 platforms.
    Also bump Loom.

---
+++ fabric-1.21.1/src/main/java/net/enderturret/patchedmod/fabric/env/ComponentWrapper.java
@@ -0,0 +1,58 @@
+package net.enderturret.patchedmod.fabric.env;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+
+import net.enderturret.patchedmod.common.env.PatchedMutableComponent;
+
+public record ComponentWrapper(MutableComponent message) implements PatchedMutableComponent {
+
+	public static MutableComponent translate(String languageKey, String message, Object... args) {
+		return Component.translatableWithFallback(languageKey, message.formatted(args), args);
+	}
+
+	@Override
+	public PatchedMutableComponent append(String languageKey, String message, Object... args) {
+		this.message.append(translate(languageKey, message, args));
+		return this;
+	}
+
+	@Override
+	public PatchedMutableComponent append(String literal) {
+		message.append(literal);
+		return this;
+	}
+
+	@Override
+	public PatchedMutableComponent appendWithCommandHover(String literal, String command, @Nullable String hoverText) {
+		Style style = Style.EMPTY
+				.withUnderlined(true)
+				.withClickEvent(new ClickEvent.SuggestCommand(command));
+
+		if (hoverText != null)
+			style = style.withHoverEvent(new HoverEvent.ShowText(Component.literal(hoverText)));
+
+		message.append(Component.literal(literal).withStyle(style));
+		return this;
+	}
+
+	@Override
+	public PatchedMutableComponent appendWithCommand(String literal, String command) {
+		message.append(Component.literal(literal).withStyle(Style.EMPTY
+				.withUnderlined(true)
+				.withClickEvent(new ClickEvent.SuggestCommand(command))));
+		return this;
+	}
+
+	@Override
+	public PatchedMutableComponent appendWithHover(String literal, String text) {
+		message.append(Component.literal(literal).withStyle(Style.EMPTY
+				.withHoverEvent(new HoverEvent.ShowText(Component.literal(text)))));
+		return this;
+	}
+}
---
+++ neoforge-1.21.1/src/main/java/net/enderturret/patchedmod/mixin/dynamic_patches/MixinPackConfig.java
@@ -0,0 +1,30 @@
+package net.enderturret.patchedmod.mixin.dynamic_patches;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.server.WorldLoader;
+import net.minecraft.server.packs.resources.CloseableResourceManager;
+
+import net.enderturret.patchedmod.common.internal.PatchTargetManager;
+import net.enderturret.patchedmod.common.internal.flow.DynamicPatches;
+import net.enderturret.patchedmod.common.util.meta.PatchedPackType;
+
+/**
+ * Handles setting up the data pack {@link PatchTargetManager}.
+ * @author EnderTurret
+ */
+@Mixin(WorldLoader.PackConfig.class)
+public abstract class MixinPackConfig {
+
+	@Inject(at = @At("RETURN"), method = "createResourceManager")
+	private void patched$setupServerPatchTargetManager(CallbackInfoReturnable<Pair<?, CloseableResourceManager>> cir) {
+		DynamicPatches.setupTargetManager(PatchedPackType.SERVER_DATA, (List) cir.getReturnValue().getSecond().listPacks().toList());
+	}
+}
