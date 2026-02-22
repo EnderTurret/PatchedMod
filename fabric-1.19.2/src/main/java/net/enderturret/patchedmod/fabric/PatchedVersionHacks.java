514887d9b3f06a126fd8f1c3919ddf1c996538ab
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2026-02-22 04:10:21 -0500

    Remove version hacks on 1.19.2.

--- fabric-1.19.2/src/main/java/net/enderturret/patchedmod/fabric/PatchedVersionHacks.java
+++ fabric-1.19.2/src/main/java/net/enderturret/patchedmod/fabric/PatchedVersionHacks.java
@@ -1,29 +1,11 @@
 package net.enderturret.patchedmod.fabric;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.concurrent.Executor;
 import java.util.zip.ZipFile;
 
 import org.jetbrains.annotations.ApiStatus.Internal;
-import org.jetbrains.annotations.Nullable;
 
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.MappingResolver;
-
-import net.minecraft.Util;
-import net.minecraft.core.Registry;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.HoverEvent;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.FilePackResources;
 
-import net.enderturret.patchedmod.common.internal.PatchedInternal;
-import net.enderturret.patchedmod.common.internal.env.PatchedPlatform;
 import net.enderturret.patchedmod.mixin.command.FilePackResourcesAccess;
 
 /**
@@ -34,47 +16,6 @@ import net.enderturret.patchedmod.mixin.command.FilePackResourcesAccess;
 public final class PatchedVersionHacks {
 
 	public static ZipFile getOrCreateZipFile(FilePackResources pack) {
-		if (zipFileAccess != null)
-			try {
-				final Object access = zipFileAccess.invoke(pack);
-				return (ZipFile) getOrCreateZipFile.invoke(access);
-			} catch (Throwable e) {
-				throw new RuntimeException(e);
-			}
-
 		return ((FilePackResourcesAccess) pack).patched$getOrCreateZipFile();
-	}
-
-	private static final @Nullable MethodHandle zipFileAccess;
-	private static final @Nullable MethodHandle getOrCreateZipFile;
-
-	static {
-		final MappingResolver mappings = FabricLoader.getInstance().getMappingResolver();
-		MethodHandle tempField = null;
-		MethodHandle tempMethod = null;
-
-		if (PatchedPlatform.get().isModLoaded("minecraft", "1.20.2")) {
-			final String filePackResourcesInt = "net.minecraft.class_3258";
-			final String zipFileAccessInt = "field_45038", zipFileAccessDescInt = "Lnet/minecraft/class_3258/class_8616;";
-			final String sharedZipFileAccessInt = "net.minecraft.class_3258.class_8616";
-			final String getOrCreateZipFileInt = "method_52426", getOrCreateZipFileDescInt = "()Ljava/util/zip/ZipFile;";
-			try {
-				final Field zipFileAccess = FilePackResources.class.getDeclaredField(mappings.mapFieldName("intermediary", filePackResourcesInt, zipFileAccessInt, zipFileAccessDescInt));
-				zipFileAccess.setAccessible(true);
-				tempField = MethodHandles.publicLookup().unreflectGetter(zipFileAccess);
-
-				final Class<?> sharedZipFileAccess = Class.forName(mappings.mapClassName("intermediary", sharedZipFileAccessInt),
-						false, PatchedVersionHacks.class.getClassLoader());
-				final Method getOrCreateZipFile = sharedZipFileAccess.getDeclaredMethod(mappings.mapMethodName("intermediary", sharedZipFileAccessInt, getOrCreateZipFileInt, getOrCreateZipFileDescInt));
-				getOrCreateZipFile.setAccessible(true);
-				tempMethod = MethodHandles.publicLookup().unreflect(getOrCreateZipFile);
-				PatchedInternal.LOGGER.debug("Found SharedZipFileAccess.getOrCreateZipFile(): {}", getOrCreateZipFile);
-			} catch (Exception e) {
-				PatchedInternal.LOGGER.warn("Exception locating SharedZipFileAccess.getOrCreateZipFile():", e);
-			}
-		}
-
-		zipFileAccess = tempField;
-		getOrCreateZipFile = tempMethod;
 	}
 }
--- forge-1.19.2/src/main/java/net/enderturret/patchedmod/forge/PatchedVersionHacks.java
+++ forge-1.19.2/src/main/java/net/enderturret/patchedmod/forge/PatchedVersionHacks.java
@@ -1,33 +1,16 @@
 package net.enderturret.patchedmod.forge;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.concurrent.Executor;
 import java.util.zip.ZipFile;
 
 import org.jetbrains.annotations.ApiStatus.Internal;
-import org.jetbrains.annotations.Nullable;
 
-import net.minecraft.Util;
-import net.minecraft.core.Registry;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
-import net.minecraftforge.resource.DelegatingPackResources;
-
-import net.enderturret.patchedmod.common.internal.PatchedInternal;
-import net.enderturret.patchedmod.common.internal.env.PatchedPlatform;
 import net.enderturret.patchedmod.mixin.command.FilePackResourcesAccess;
 import net.enderturret.patchedmod.mixin.forge.DelegatingPackResourcesAccess;
 
@@ -39,14 +22,6 @@ import net.enderturret.patchedmod.mixin.forge.DelegatingPackResourcesAccess;
 public final class PatchedVersionHacks {
 
 	public static ZipFile getOrCreateZipFile(FilePackResources pack) {
-		if (zipFileAccess != null)
-			try {
-				final Object access = zipFileAccess.invoke(pack);
-				return (ZipFile) getOrCreateZipFile.invoke(access);
-			} catch (Throwable e) {
-				throw new RuntimeException(e);
-			}
-
 		return ((FilePackResourcesAccess) pack).patched$getOrCreateZipFile();
 	}
 
@@ -55,31 +30,5 @@ public final class PatchedVersionHacks {
 		if (pack instanceof DelegatingPackResourcesAccess access)
 			return access.patched$getCandidatePacks(type, new ResourceLocation(namespace, ""));
 		return Collections.emptyList();
-	}
-
-	private static final @Nullable MethodHandle zipFileAccess;
-	private static final @Nullable MethodHandle getOrCreateZipFile;
-
-	static {
-		MethodHandle tempField = null;
-		MethodHandle tempMethod = null;
-
-		if (PatchedPlatform.get().isModLoaded("minecraft", "1.20.2")) {
-			try {
-				final Field zipFileAccess = ObfuscationReflectionHelper.findField(FilePackResources.class, "f_291183_");
-				tempField = MethodHandles.publicLookup().unreflectGetter(zipFileAccess);
-
-				final Class<?> sharedZipFileAccess = Class.forName("net.minecraft.server.packs.FilePackResources$SharedZipFileAccess",
-						false, PatchedVersionHacks.class.getClassLoader());
-				final Method getOrCreateZipFile = ObfuscationReflectionHelper.findMethod(sharedZipFileAccess, "m_295521_");
-				tempMethod = MethodHandles.publicLookup().unreflect(getOrCreateZipFile);
-				PatchedInternal.LOGGER.debug("Found SharedZipFileAccess.getOrCreateZipFile(): {}", getOrCreateZipFile);
-			} catch (Exception e) {
-				PatchedInternal.LOGGER.warn("Exception locating SharedZipFileAccess.getOrCreateZipFile():", e);
-			}
-		}
-
-		zipFileAccess = tempField;
-		getOrCreateZipFile = tempMethod;
 	}
 }
