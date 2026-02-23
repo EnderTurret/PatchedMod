efc2112c4031b414897fab9db3717499369381d1
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2026-02-22 23:25:45 -0500

    Fix `MixinFallbackResourceManager` compile errors.

--- fabric-1.18.2/src/main/java/net/enderturret/patchedmod/mixin/impl/MixinFallbackResourceManager.java
+++ fabric-1.18.2/src/main/java/net/enderturret/patchedmod/mixin/impl/MixinFallbackResourceManager.java
@@ -1,10 +1,12 @@
 package net.enderturret.patchedmod.mixin.impl;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -14,7 +16,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.FallbackResourceManager;
-import net.minecraft.server.packs.resources.Resource;
 
 import net.enderturret.patchedmod.common.env.PatchedPackResources;
 import net.enderturret.patchedmod.common.env.PatchedResourceLocation;
@@ -22,47 +23,62 @@ import net.enderturret.patchedmod.common.env.PatchedResourceManager;
 import net.enderturret.patchedmod.common.internal.flow.PatchingManager;
 import net.enderturret.patchedmod.common.util.PatchUtil;
 import net.enderturret.patchedmod.common.util.meta.PatchedPackType;
-import net.enderturret.patchedmod.fabric.FallbackResourceManagerAccess;
 
 /**
  * This mixin implements the functionality for actually patching resources.
  * @author EnderTurret
  */
 @Mixin(FallbackResourceManager.class)
-public abstract class MixinFallbackResourceManager implements FallbackResourceManagerAccess {
+public abstract class MixinFallbackResourceManager {
 
 	@Shadow
 	@Final
 	private PackType type;
 
 	@WrapOperation(
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/FallbackResourceManager;createResourceGetter("
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/FallbackResourceManager;getWrappedResource("
 					+ "Lnet/minecraft/resources/ResourceLocation;"
 					+ "Lnet/minecraft/server/packs/PackResources;"
-					+ ")Lnet/minecraft/server/packs/resources/Resource$IoSupplier;"),
-			method = { "getResource", "listResources" })
-	private Resource.IoSupplier<InputStream> patched$replaceResourceMulti(
+					+ ")Ljava/io/InputStream;"),
+			method = { "getResource" })
+	private InputStream patched$replaceResourceMulti(
 			FallbackResourceManager _self,
 			ResourceLocation location, PackResources pack,
-			Operation<Resource.IoSupplier<InputStream>> downstream) {
+			Operation<InputStream> downstream) throws IOException {
 		final FallbackResourceManager self = (FallbackResourceManager) (Object) this;
-		final Resource.IoSupplier<InputStream> streamSupplier = downstream.call(_self, location, pack);
-		return patched$chain(streamSupplier, (PatchedResourceLocation) location, (PatchedPackResources) pack, false);
+		final InputStream stream = downstream.call(_self, location, pack);
+		return patched$chain(stream, (PatchedResourceLocation) location, (PatchedPackResources) pack, false);
+	}
+
+	@WrapOperation(
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/FallbackResourceManager;getWrappedResource("
+					+ "Lnet/minecraft/resources/ResourceLocation;"
+					+ "Lnet/minecraft/server/packs/PackResources;"
+					+ ")Ljava/io/InputStream;"),
+			method = { "getResources" })
+	private InputStream patched$replaceResourceSinglePack(
+			FallbackResourceManager _self,
+			ResourceLocation location, PackResources pack,
+			Operation<InputStream> downstream) throws IOException {
+		final FallbackResourceManager self = (FallbackResourceManager) (Object) this;
+		final InputStream stream = downstream.call(_self, location, pack);
+		return patched$chain(stream, (PatchedResourceLocation) location, (PatchedPackResources) pack, true);
 	}
 
 	/**
-	 * "Chains" the given {@code IoSupplier}, returning an {@code IoSupplier} that patches the data returned by it.
-	 * @param delegate The delegate {@code IoSupplier}.
+	 * "Chains" the given {@code InputStream}, returning an {@code InputStream} that patches the data returned by it.
+	 * @param delegate The delegate {@code InputStream}.
 	 * @param name The location of the data.
 	 * @param origin The resource or data pack that the data originated from.
 	 * @param singlePack Whether or not only patches from the pack containing the resource should be applied.
-	 * @return The new {@code IoSupplier}.
+	 * @return The new {@code InputStream}.
+	 * @throws IOException
 	 */
-	@Override
-	public Resource.IoSupplier<InputStream> patched$chain(Resource.IoSupplier<InputStream> delegate, PatchedResourceLocation name, PatchedPackResources origin, boolean singlePack) {
+	@Unique
+	private InputStream patched$chain(InputStream delegate, PatchedResourceLocation name, PatchedPackResources origin, boolean singlePack) throws IOException {
 		if (!PatchUtil.isPatchable(name.patched$getPath())) return delegate;
 		final PatchedPackType type = this.type == PackType.CLIENT_RESOURCES ? PatchedPackType.CLIENT_RESOURCES : PatchedPackType.SERVER_DATA;
 		final PatchedResourceManager manager = (PatchedResourceManager) this;
-		return () -> PatchingManager.newPatchingStream(delegate.get(), manager, origin, type, name, singlePack);
+		return PatchingManager.newPatchingStream(delegate, manager, origin, type, name, singlePack);
 	}
 }
